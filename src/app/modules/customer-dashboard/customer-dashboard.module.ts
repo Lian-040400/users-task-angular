194b319197f2bb49923af35ccbd37769de995fd1
Lianna <xacatryanlianna630@gmail.com>
2021-11-09 08:50:29 +0400

    com

--- src/app/modules/customer-dashboard/customer-dashboard.module.ts
+++ src/app/modules/customer-dashboard/customer-dashboard.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AddUserComponent } from './components/add-user/add-user.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
 
 @NgModule({
   declarations: [
     AddUserComponent,
     HeaderComponent,
     UserDashboardComponent,
+    UserCardComponent,
   ],
   imports: [
     CommonModule
